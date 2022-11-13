d811fe5161423ad27dc845503a60821ede6a43a4
Matheus Sales Da Silva <78692912+matheussdsi@users.noreply.github.com>
2022-11-13 12:16:42 -0300

    Add files via upload

---
+++ Licenciamento.ps1
@@ -0,0 +1,36 @@
+#1º#
+Connect-MsolService
+#2º# 
+Get-MsolAccountsku
+
+#Verificar usuários Licenciados#
+Get-MsolUser | Where {$_.IsLicensed}|format-table
+
+Get-MsolUser | Where {$_.IsLicensed}| Out-GridView
+
+Get-MsolUser | Where {$_.IsLicensed}| Select status ,Displayname ,Licenses |Out-GridView
+
+Get-MsolUser | Where {$_.IsLicensed}| Select status ,Displayname ,Licenses |
+#Verificar usuários Licenciados#
+Get-MsolUser | Where {($_.IsLicensed).AccountSkuID -eq $accountskuid} | Export-CSV "C:\tmp\licenças.csv"
+
+Get-MsolUser | Where {($_.IsLicensed).AccountSkuID -eq $accountskuid} | Export-CSV "C:\tmp\licenças.csv"
+
+Get-MsolUser | Where {($_.Licenses).AccountSkuID -eq $accountskuid} | Export-CSV "C:\tmp\licenças.csv"
+
+#Visualizar se usuarios estão licenciados com campos personalizaveis#
+Get-MsolUser -All| Select DisplayName,Department,IsLicensed,UsageLocation |export-csv "c:\tmp\bomba.csv"
+
+Get-MsolUser -All| Select DisplayName,Department,IsLicensed,UsageLocation |Out-GridView
+
+
+Get-MgUser -All | Select DisplayName,UserPrincipalName,JobTitle| Export-CSV -Path "C:\ExportDetails.CSV" -NoTypeInformation
+
+
+Get-MsolUser -All | Select DisplayName,Department,IsLicensed| Export-CSV -Path "C:\ExportDetails.CSV"  -NoTypeInformation
+
+Get-MgSubscribedSku
+
+
+
+
